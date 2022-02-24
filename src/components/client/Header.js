35bd0918781adcc56690a8dceaac55cefe6a5b44
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-02-24 05:44:10 +0100

    adds menu to services tab, blends it tab background

--- src/components/client/Header.js
+++ src/components/client/Header.js
@@ -8,6 +8,8 @@ import { Tabs } from "@material-ui/core";
 import { Tab } from "@material-ui/core";
 import { Button } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import { Menu } from "@material-ui/core";
+import { MenuItem } from "@material-ui/core";
 
 import logo from "../../assets/logo.svg";
 
@@ -53,36 +55,72 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: "50px",
     height: "45px",
   },
+  menu: {
+    background: theme.palette.common.blue,
+    color: "white",
+  },
+  menuItem: {
+    ...theme.typography.tab,
+    opacity: 0.7,
+    "&:hover": {
+      opacity: 1,
+    },
+  },
 }));
 
 function Header() {
   const classes = useStyles();
   const [value, setValue] = useState(0);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [open, setOpen] = useState(false);
+
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+    setOpen(false);
+  };
 
   const handleChange = (e, value) => {
     setValue(value);
   };
 
-  useEffect(() => { 
-    if (window.location.pathname === "/" && value !== 0){
-      setValue(0)
+  // This is important to retain the active navigation tab after refresh
+  useEffect(() => {
+    if (window.location.pathname === "/" && value !== 0) {
+      setValue(0);
     } else if (window.location.pathname === "/services" && value !== 1) {
-      setValue(1)
+      setValue(1);
+    } else if (window.location.pathname === "/customSoftware" && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === "/mobileApps" && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === "/websites" && value !== 1) {
+      setValue(1);
     } else if (window.location.pathname === "/revolution" && value !== 2) {
-      setValue(2)
+      setValue(2);
     } else if (window.location.pathname === "/about" && value !== 3) {
-      setValue(3)
+      setValue(3);
     } else if (window.location.pathname === "/contact" && value !== 4) {
-      setValue(4)
+      setValue(4);
     }
-  }, [value])
+  }, [value]);
 
   return (
     <>
       <ElevationScroll>
         <AppBar position="fixed">
           <Toolbar disableGutters>
-            <Button className={classes.logoContainer} component={Link} to="/" onClick={()=> setValue(0)} disableRipple>
+            <Button
+              className={classes.logoContainer}
+              component={Link}
+              to="/"
+              onClick={() => setValue(0)}
+              disableRipple
+            >
               <img className={classes.logo} src={logo} alt="Company logo" />
             </Button>
 
@@ -100,10 +138,13 @@ function Header() {
               />
 
               <Tab
+                aria-owns={anchorEl ? "simple-menu" : undefined}
+                aria-haspopup={anchorEl ? "true" : undefined}
                 label="Services"
                 component={Link}
                 to="/services"
                 className={classes.tab}
+                onMouseOver={handleClick}
               />
 
               <Tab
@@ -134,6 +175,60 @@ function Header() {
             >
               Free Estimate
             </Button>
+            <Menu
+              aria-owns="simple-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{ onMouseLeave: handleClose }}
+              classes={{ paper: classes.menu }}
+              elevation={0}
+            >
+              <MenuItem
+                classes={{ root: classes.menuItem }}
+                onClick={() => {
+                  handleClose();
+                  setValue(1);
+                }}
+                component={Link}
+                to="/services"
+              >
+                Services
+              </MenuItem>
+              <MenuItem
+                classes={{ root: classes.menuItem }}
+                onClick={() => {
+                  handleClose();
+                  setValue(1);
+                }}
+                component={Link}
+                to="/customSoftware"
+              >
+                Custom Software Development
+              </MenuItem>
+              <MenuItem
+                classes={{ root: classes.menuItem }}
+                onClick={() => {
+                  handleClose();
+                  setValue(1);
+                }}
+                component={Link}
+                to="/mobileApps"
+              >
+                Mobile Applications
+              </MenuItem>
+              <MenuItem
+                classes={{ root: classes.menuItem }}
+                onClick={() => {
+                  handleClose();
+                  setValue(1);
+                }}
+                component={Link}
+                to="/websites"
+              >
+                Website Development
+              </MenuItem>
+            </Menu>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
